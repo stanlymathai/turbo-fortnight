467fbefa83666b7653640149d45a8697bf593008
rakesh.kumar-at-354455067292 <rakesh@infinexpartners.com>
2023-09-28 16:14:27 +0530

    get prefence added

--- src/controllers/preferencesTag.controller.js
+++ src/controllers/preferencesTag.controller.js
@@ -56,9 +56,24 @@ async function getTagList(req, res) {
 
 async function getUserTagList(req, res) {
   try {
-    const persons = await getUserTagData(req);
-    res.json(persons);
+    const result = await getUserTagData(req);
+    if(result.status){
+      const responseData ={
+        "success": true,
+        'msg':constants.MESSAGES.DATA_FOUND,
+        "data":result.data
+      }
+     res.status(200).json(responseData);
+    }
+    else{
+      const responseData ={
+        "success": false,
         'msg':constants.MESSAGES.DATA_NOT_FOUND,
+        "data":result.data
+      }
+     res.status(200).json(responseData);
+    }
+       
   } catch (error) {
     console.error('Error fetching persons:', error);
     res.status(500).send('An error occurred while processing your request.');
